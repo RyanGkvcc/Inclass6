760da834ef927d78a223e6498ed8952050520296
Ryan Gowan <tr_jester@hotmail.com>
2016-04-20 19:05:44 -0400

    Add Foo page, and method in home controller to handle

--- cis237inclass6/App_Start/RouteConfig.cs
+++ cis237inclass6/App_Start/RouteConfig.cs
@@ -13,13 +13,31 @@ namespace cis237inclass6
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //Most specific to most generic url
-            routes.MapRoute(
-                name: "Foo",
-                url: "{controller}/foo",
-                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
-            );
+            //Could use this route if you wanted to. It will work.
+            //We just don't need it so I commented it out.
 
+            //Most specific to most generic url
+            //routes.MapRoute(
+            //    name: "Foo",
+            //    url: "Foo",
+            //    defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            //);            
+            
+            //Most specific to most generic url
+            //routes.MapRoute(
+            //    name: "Foo",
+            //    url: "{controller}/foo",
+            //    defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            //);
+
+            //Most specific to most generic url
+            //routes.MapRoute(
+            //    name: "Foo",
+            //    url: "{controller}/Foo",
+            //    defaults: new { controller = "Home", action = "Foo", id = UrlParameter.Optional }
+            //);
+
+ 
             //More generic url
             routes.MapRoute(
                 name: "Default",
